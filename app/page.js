6f65bb1336de156f8b682dd36f98f481e86bfd5f
mehdisoheilinia5-debug <mehdisoheilinia5@gmail.com>
2026-06-26 23:46:20 +0330

    Delete page.js
    delete duplicate

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page() {
-  return (
-    <div style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>🎭 TheaterGram</h1>
-      <p>در حال راه‌اندازی...</p>
-    </div>
-  );
-} 
