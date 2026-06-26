c551cf8c3fbd2e29c8c76a8881b2284869dcd1bd
mehdisoheilinia5-debug <mehdisoheilinia5@gmail.com>
2026-06-27 02:58:12 +0330

    Delete contants.ts

--- app/contants.ts
+++
@@ -1,6 +0,0 @@
-export const categories = [
-  { id: 'educational', fa: 'آموزشی', en: 'Educational' },
-  { id: 'rehearsal', fa: 'تمرین', en: 'Rehearsal' },
-  { id: 'improvisation', fa: 'بداهه', en: 'Improvisation' },
-  { id: 'etude', fa: 'اتود', en: 'Etude' }
-];
