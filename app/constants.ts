e8d6dd472b10fa01eb08ff18b3cd591d0d0242e4
mehdisoheilinia5-debug <mehdisoheilinia5@gmail.com>
2026-06-27 03:28:01 +0330

    Create constants.ts

---
+++ app/constants.ts
@@ -0,0 +1,6 @@
+export const categories = [
+  { id: 'educational', fa: 'آموزشی', en: 'Educational' },
+  { id: 'rehearsal', fa: 'تمرین', en: 'Rehearsal' },
+  { id: 'improvisation', fa: 'بداهه', en: 'Improvisation' },
+  { id: 'etude', fa: 'اتود', en: 'Etude' }
+];
