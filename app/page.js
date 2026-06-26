84498ae0536891275b66cd6202d067be4b76404c
mehdisoheilinia5-debug <mehdisoheilinia5@gmail.com>
2026-06-26 23:26:41 +0330

    Create app/page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>🎭 TheaterGram</h1>
+      <p>در حال راه‌اندازی...</p>
+    </div>
+  );
+}
